5f15a8dfc5e96ba5090bbc8c935d4e01f5be620e
Mateusz Kaszubowski <m.aizenvolt@gmail.com>
2025-07-16 21:51:05 +0200

    Initial commit

---
+++ src/main/java/pl/edu/pg/chor/strefa_chorzysty/StrefaChorzystyApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.chor.strefa_chorzysty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrefaChorzystyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrefaChorzystyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pg/chor/strefa_chorzysty/StrefaChorzystyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.chor.strefa_chorzysty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrefaChorzystyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
